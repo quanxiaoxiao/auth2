9386deae48f5e2fb6fa71669490652526c8407d9
quan <quan_xiaoxiao@qq.com>
2024-02-25 16:58:03 +0800

    xxx

--- scripts/routeMatch.mjs
+++ scripts/routeMatch.mjs
@@ -1,6 +1,7 @@
 import assert from 'node:assert';
 import { http } from '@quanxiaoxiao/about-net';
 import { decodeContentToJSON } from '@quanxiaoxiao/http-utils';
+import { ACCOUNT_TYPE_TEST } from '../src/constants.mjs';
 
 const port = 4037;
 const hostname = '127.0.0.1';
@@ -146,6 +147,84 @@ const removeRouteMatchGroup = async (routeMatchGroup) => {
   return null;
 };
 
+const createAccount = async ({
+  username,
+  password,
+  ...other
+}) => {
+  const requestRet = await http.httpRequest({
+    hostname,
+    port,
+    method: 'POST',
+    path: '/authapi/account',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      type: ACCOUNT_TYPE_TEST,
+      ...other,
+      username,
+      password,
+    }),
+  });
+  if (requestRet.statusCode === 200) {
+    const data = await decodeContentToJSON(requestRet.body, requestRet.headers);
+    return data;
+  }
+  return null;
+};
+
+const updateAccount = async ({
+  account,
+  data,
+}) => {
+  const requestRet = await http.httpRequest({
+    hostname,
+    port,
+    method: 'PUT',
+    path: `/authapi/account/${account}`,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      ...data,
+    }),
+  });
+  assert(requestRet.statusCode === 200);
+  const ret = await decodeContentToJSON(requestRet.body, requestRet.headers);
+  return ret;
+};
+
+const removeAccount = async (account) => {
+  const requestRet = await http.httpRequest({
+    hostname,
+    port,
+    method: 'DELETE',
+    path: `/authapi/account/${account}`,
+    body: null,
+  });
+  if (requestRet.statusCode === 200) {
+    const data = await decodeContentToJSON(requestRet.body, requestRet.headers);
+    return data;
+  }
+  return null;
+};
+
+const getAccount = async (account) => {
+  const requestRet = await http.httpRequest({
+    hostname,
+    port,
+    method: 'GET',
+    path: `/authapi/account/${account}`,
+    body: null,
+  });
+  if (requestRet.statusCode === 200) {
+    const data = await decodeContentToJSON(requestRet.body, requestRet.headers);
+    return data;
+  }
+  return null;
+};
+
 const pipeline = async () => {
   let routeMatchItem = await createRouteMatch({
     path: 'aaa',
@@ -209,10 +288,81 @@ const pipeline = async () => {
   assert(routeMatchGroupItem);
   assert(routeMatchGroupItem.routeMatches[0] === routeMatchItem._id);
 
+  let accountItem = await createAccount({
+    username: 'test_aaa',
+    password: '123',
+  });
+
+  assert(accountItem);
+  assert(!accountItem.routeMatchGroups.includes(routeMatchGroupItem._id));
+
+  await removeAccount(accountItem._id);
+
+  routeMatchGroupItem = await updateRouteMatchGroup({
+    routeMatchGroup: routeMatchGroupItem._id,
+    data: {
+      isSetDefault: true,
+    },
+  });
+
+  assert(routeMatchGroupItem);
+
+  accountItem = await createAccount({
+    username: 'test_aaa',
+    password: '123',
+  });
+
+  assert(accountItem);
+  assert(accountItem.routeMatchGroups.includes(routeMatchGroupItem._id));
+
+  routeMatchGroupItem = await updateRouteMatchGroup({
+    routeMatchGroup: routeMatchGroupItem._id,
+    data: {
+      isSetDefault: false,
+    },
+  });
+  assert(routeMatchGroupItem);
+
+  accountItem = await getAccount(accountItem._id);
+
+  assert(accountItem);
+
+  assert(accountItem.routeMatchGroups.includes(routeMatchGroupItem._id));
+
+  let accountItem2 = await createAccount({
+    username: 'test_aaa2',
+    password: '123',
+  });
+
+  assert(accountItem2);
+  assert(!accountItem2.routeMatchGroups.includes(routeMatchGroupItem._id));
+
+  routeMatchGroupItem = await updateRouteMatchGroup({
+    routeMatchGroup: routeMatchGroupItem._id,
+    data: {
+      isSetDefault: true,
+    },
+  });
+  assert(routeMatchGroupItem);
+
+  accountItem2 = await getAccount(accountItem2._id);
+
+  assert(accountItem2);
+  assert(!accountItem2.routeMatchGroups.includes(routeMatchGroupItem._id));
+
+  await removeAccount(accountItem2._id);
+
   routeMatchGroupItem = await removeRouteMatchGroup(routeMatchGroupItem._id);
   assert(routeMatchGroupItem);
+  accountItem = await getAccount(accountItem._id);
+  assert(accountItem);
+
+  assert(!accountItem.routeMatchGroups.includes(routeMatchGroupItem._id));
+
   routeMatchItem = await removeRouteMatch(routeMatchItem._id);
   assert(routeMatchItem);
+
+  await removeAccount(accountItem._id);
 };
 
 await pipeline();
